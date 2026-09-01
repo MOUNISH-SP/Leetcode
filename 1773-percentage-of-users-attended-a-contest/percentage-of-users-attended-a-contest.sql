094b6766a8f8f83d6be88e0a74e9e4d8e276d4a3
MOUNISH-SP <mounishsp.23csd@kongu.edu>
2026-09-01 19:33:52 +0530

    Time: 997 ms (72.69%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select r.contest_id, Round((count(r.user_id)*100)/(select count(*)from Users),2) as percentage from Register as r inner join Users as u on r.user_id=u.user_id group by r.contest_id 
+select r.contest_id, Round((count(r.user_id)*100)/(select count(*)from Users),2) as percentage from Register as r cross join Users as u on r.user_id=u.user_id group by r.contest_id 
 Order by percentage DESC ,r.contest_id ASC;
