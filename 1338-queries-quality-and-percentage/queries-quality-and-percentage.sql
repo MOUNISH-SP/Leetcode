e0352d535cf97f7f3f86393cccc7b36b4989d28d
MOUNISH-SP <mounishsp.23csd@kongu.edu>
2026-09-01 22:21:03 +0530

    Time: 405 ms (47.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select query_name,Round(avg(rating/position),2) as quality,Round(avg(if(rating <3,1,0))*100,2) as poor_query_percentage from Queries group by query_name;
