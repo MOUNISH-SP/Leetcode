6d3f4e3cb446d8e89caa34ae73058f0d49e2dabe
MOUNISH-SP <mounishsp.23csd@kongu.edu>
2026-09-12 20:17:05 +0530

    Time: 363 ms (67.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+select employee_id from Employees where salary<30000 and manager_id is not null and manager_id not in (select employee_id from Employees) order by employee_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+#Select employee_id from Employees
+# where salary < 30000
+# and manager_id is not null
+# and manager_id not in (Select employee_id from Employees)
+# order by employee_id
