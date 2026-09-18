24af3a9688fe3371c76542c019b068babefd7290
MOUNISH-SP <mounishsp.23csd@kongu.edu>
2026-09-18 21:35:44 +0530

    Time: 1 ms (99.98%) | Memory: 43.7 MB (22.75%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sd=new StringBuilder();
+        int n=a.length()-1;
+        int m=b.length()-1;
+        int carry=0;
+
+        while(n>=0 || m>=0 || carry>0){
+            int sum=carry;
+            if(n>=0){
+                sum+=a.charAt(n--) - '0';
+            }
+            if(m>=0){
+                sum+=b.charAt(m--) - '0';
+            }
+
+            sd.append(sum%2);
+            carry=sum/2;
+        }
+        return sd.reverse().toString();
+
+
+
+
+
+    }
+}
