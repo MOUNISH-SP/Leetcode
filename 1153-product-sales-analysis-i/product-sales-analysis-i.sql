5823354110d561e436c94a7c0aa16d5f6fc14d1a
MOUNISH-SP <mounishsp.23csd@kongu.edu>
2026-09-22 09:34:24 +0530

    Time: 1200 ms (91.56%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,7 @@
-# Write your MySQL query statement below
-select p.product_name,s.year,s.price from Product as p  right join Sales as s on p.product_id=s.product_id; 
+
+
+
+SELECT p.product_name, s.year, s.price
+FROM Sales AS s
+LEFT JOIN Product AS p
+ON s.product_id = p.product_id;
